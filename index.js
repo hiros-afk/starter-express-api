57205816040ba4aae21c42126c2a6f13fe621d48
hiros-afk <136979919+hiros-afk@users.noreply.github.com>
2023-06-18 21:17:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+
+// Create a connection pool
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'my_database',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// Run a query to retrieve data
+app.get('/users', (req, res) => {
+  pool.query('SELECT * FROM users', (error, results) => {
+    if (error) {
+      return res.status(500).json({ error });
+    }
+    return res.json(results);
+  });
+});
+
+// Catch-all handler for all other requests
+app.use('*', (req, res) => {
+  res.json({ msg: 'no route handler found' }).end();
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
